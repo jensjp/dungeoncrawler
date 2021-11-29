f404e8d9bdf7de76726175c288ac39478075b1c6
Jens J Parappallil <jens.parappallil@ibsplc.com>
2021-11-29 12:48:58 +0530

    Added Health and attacks

--- src/systems/random_move.rs
+++ src/systems/random_move.rs
@@ -3,8 +3,11 @@ use crate::prelude::*;
 #[system]
 #[read_component(Point)]
 #[read_component(MoveRandomly)]
+#[read_component(Health)]
+#[read_component(Player)]
 pub fn random_move(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
   let mut movers = <(Entity, &Point, &MoveRandomly)>::query();
+  let mut positions = <(Entity, &Point, &Health)>::query();
   movers.iter(ecs).for_each(|(entity, pos, _)| {
     let mut rng = RandomNumberGenerator::new();
     let destination = match rng.range(0, 4) {
@@ -14,6 +17,17 @@ pub fn random_move(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
       3 => Point::new(0, 1),
       _ => Point::zero(),
     } + *pos;
-    commands.push(((), WantsToMove { entity: *entity, destination }));
+    let mut attacked = false;
+    positions.iter(ecs)
+      .filter(|(_, pos, _)| **pos == destination)
+      .for_each(|(victim, _, _)| {
+        if ecs.entry_ref(*victim).unwrap().get_component::<Player>().is_ok() {
+          commands.push(((), WantsToAttack{attacker: *entity, victim: *victim}));
+        }
+        attacked = true;
+      });
+    if !attacked {
+      commands.push(((), WantsToMove { entity: *entity, destination }));
+    }
   });
 }
